5c4f35f2865a5a326334d9c6e5b705e4b7b40a46
Ahmed Abdelfattah <ahmedomar1007@gmail.com>
2023-03-06 17:36:53 +0200

    contact

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ router.get('/works', (req, res) => res.render('pages/works'))
 router.get('/about', (req, res) => res.render('pages/about'))
 router.get('/snippets', (req, res) => res.render('pages/snippets'))
 router.get('/contact', (req, res) => res.render('pages/contact'))
+router.get('/thanks', (req, res) => res.render('pages/thanks'))
 router.get('/*', (req, res) => res.render('pages/404'))
 
 export default router
